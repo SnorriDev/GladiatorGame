f4fb36bcea9a722d5ccffb61456f21647db91de6
Will M <vikingarnir.will@gmail.com>
2014-05-28 23:32:30 -0400

    animations and ragdolls

---
+++ Assets/AnimationManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour {
+
+	public AnimationClip walking;
+	public AnimationClip running;
+	public AnimationClip hurtWalking;
+	public AnimationClip idle;
+
+	public float walkSpeed;
+	public float runSpeed;
+
+	//maybe get rid of running?
+	
+	private CharacterController characterController;
+
+	void Start () {
+		characterController = transform.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (characterController.velocity.magnitude < walkSpeed)
+			animation.CrossFade (idle.name);
+		else if (characterController.velocity.magnitude < runSpeed)
+			animation.CrossFade (walking.name);
+		else
+			animation.CrossFade (running.name);
+
+		characterController.Move (transform.forward * (Input.GetKey(KeyCode.Z) ? 1 : 0) * Time.deltaTime);
+
+	}
+}
---
+++ Assets/KillRagdoll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillRagdoll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		kill<BoxCollider> ();
+	}
+
+	private void kill<E>() where E : Collider {
+		foreach (E r in transform.GetComponentsInChildren<E>()) {
+			Debug.Log(r);
+			((Collider) r).isTrigger = true;
+		}
+
+	}
+
+}
