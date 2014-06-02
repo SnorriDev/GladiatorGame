794f76194201156188f3fcc5410c6e29680a2dc4
legotack <legotack@gmail.com>
2014-06-02 18:01:40 -0400

    last commit didnt work, added RefreshGraph.cs

---
+++ Assets/RefreshGraph.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RefreshGraph : MonoBehaviour {
+
+	private Vector3 initialPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Vector3.forward * Time.deltaTime);
+		if (Vector3.Distance (initialPos, transform.position) > 1) {
+			AstarPath.active.UpdateGraphs(collider.bounds);
+			initialPos = transform.position;
+		}
+	}
+}
