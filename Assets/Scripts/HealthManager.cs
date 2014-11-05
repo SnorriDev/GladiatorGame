0c5eb1ca862e6cb31ceb500aa320453bce3f801f
Will M <vikingarnir.will@gmail.com>
2014-11-04 19:14:37 -0500

    Started new damage system and put in the basics of the hero class. This commit doesn't actually change much

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -4,19 +4,31 @@ using System.Collections;
 public class HealthManager : MonoBehaviour {
 
 	//TODO: add damage broadcast system?
+	//TODO: make a damage class
+
+	public enum DamageType {PIERCE, CRUSH};
 
 	private KillRagdoll killRagdoll;
 
 	public float maxHealth;
 	public float health;
+	public float armor;
 
 	void Start () {
 		health = maxHealth;
 		killRagdoll = GetComponent<KillRagdoll>();
 	}
 
+	//TODO: change the type of damage from explosions
 	public void damage(float amount) {
-		health -= amount;
+		damage (amount, DamageType.PIERCE);
+	}
+
+	public void damage(float amount, DamageType type) {
+		if (type == DamageType.PIERCE)
+			health -= (1 - armor) * amount;
+		else
+			health -= amount;
 	}
 
 	public bool isDead() {
